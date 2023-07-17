ca27516b946ccfee9c1a3cec47c43045746d8fe2
GaurAfro <gauraboschma@gmail.com>
2023-07-17 02:13:23 +0200

    last sync for windows for a while

--- python/Claude_2_Copy_Chat_Log/AfroMemo.py
+++ python/Claude_2_Copy_Chat_Log/AfroMemo.py
@@ -1,55 +1,85 @@
-# I want to copy the chat log from the chat that I have with any LLM and paste it into a text file,
-# The text file should be saved in the folder of the LLM.
-# There should be a different folder for each LLM.
-# The text file should be named after the LLM.
-# The text file should be named after the date of the chat.
-# The text file should be named after the time of the chat.
-
 # Imports
-import pyperclip 
+import pyperclip
 import tkinter as tk
-
+from collections import deque
 
 # Functions
 
+
+def main():
+    my_chat = ChatConversation("My Chat")
+    my_chat.add_message("Hello")
+
+    print(my_chat.messages)
+
+
 def copy_to_clipboard(text):
+    """Copy text to clipboard"""
     pyperclip.copy(text)
-    
+
+
 def paste_from_clipboard():
+    """Paste text from clipboard"""
     return pyperclip.paste()
 
-    
-# Classes 
+
+# Classes
+
 
 class ChatConversation:
-    
+    """Chat conversation class"""
+
+
     def __init__(self, title):
         self.title = title
         self.messages = []
-        
+
     def add_message(self, message):
-        """Add message to chat history"""
+        """Add message to chat"""
         self.messages.append(message)
 
 
 class ClaudeChat(ChatConversation):
 
-        def __init__(self):
-            super().__init__("Claude")
+    def __init__(self):
+        super().__init__("Claude")
 
-            
+
 class GPTChat(ChatConversation):
 
-        def __init__(self):
-             super().__init__("GPT")
-             
+    def __init__(self):
+        super().__init__("GPT")
 
-# Tkinter GUI code
 
+# Data structures
+chats = {
+    "Chat with Claude": "",
+    "Chat with GPT": ""
+}
+
+clipboard_history = deque(maxlen=100)
+pinned_items = []
+frequent_items = {
+    "example": 0
+}
+
+
+def add_to_clipboard(text):
+
+    if text in clipboard_history:
+        clipboard_history.remove(text)
+
+    clipboard_history.append(text)
+
+
+def pin_item(text):
+    pinned_items.append(text)
+
+
+# GUI
 root = tk.Tk()
-root.title("AfroMemo") 
+root.title("AfroMemo")
 
-
-# Chat objects
-my_chat_with_claude = ClaudeChat()
-my_chat_with_gpt = GPTChat()
+# Main
+if __name__ == "__main__":
+    main()
