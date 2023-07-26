d6c7cd5df94569b2011dfe5b14ff82ab279579b9
GaurAfro <gauraboschma@gmail.com>
2023-07-26 13:49:29 +0200

    sync before laptop upgrade

---
+++ bash-scripting/fonts-config.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+fonts_dir="${HOME}/.local/share/fonts"
+if [ ! -d "${fonts_dir}" ]; then
+    echo "mkdir -p $fonts_dir"
+    mkdir -p "${fonts_dir}"
+else
+    echo "Found fonts dir $fonts_dir"
+fi
+
+# FiraCode.zip
+# OpenDyslexic.zip
+# SourceCodePro.zip
+# zip=Fira_Code_v${version}.zip
+# curl --fail --location --show-error https://github.com/tonsky/FiraCode/releases/download/${version}/${zip} --output ${zip}
+# unzip -o -q -d ${fonts_dir} ${zip}
+# rm ${zip}
+
+echo "fc-cache -f"
+fc-cache -f
