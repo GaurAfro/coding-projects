6c7bd1a1e0dde6449408dd8b2232e6822501973e
gaurafro <gauraboschma@gmail.com>
2023-08-29 00:40:17 +0200

    made improvements

--- bash/md_to_org.sh
+++ bash/md_to_org.sh
@@ -2,20 +2,20 @@
 
 # Loop through all .md files in the current directory
 for input_file in *.md; do
-  # Skip if no .md files found
-  [[ ! -e $input_file ]] && echo "No .md files found" && continue
+    # Skip if no .md files found
+    [[ ! -e $input_file ]] && echo "No .md files found" && continue
 
   # Generate output filename by replacing .md extension with .org
   output_file="${input_file%.md}.org"
 
   # Perform the conversion
-  sed -e 's/^# \(.*\)$/ * \1/' \
+  sed \
+      -e 's/^# \(.*\)$/ * \1/' \
       -e 's/^## \(.*\)$/ ** \1/' \
       -e 's/^### \(.*\)$/ *** \1/' \
-      -e 's/\*\*\(.*\)\*\$/*\1*/' \
-      -e 's/\*\(.*\)\*\$/\1/' \
+      -e 's/\*\*\(.*\)\*\*/\*\1\*/g' \
+      -e 's/^\([ \t]*\)`\{3\}$/\1#+END_SRC/' \
       -e 's/^\([ \t]*\)`\{3\}\([^ ]*\)/\1#+BEGIN_SRC \2/' \
-      -e 's/^\([ \t]*\)`\{3\}/\1#+END_SRC/' \
       -e 's/\[\([^\]]*\)\]\([^\)]*\)/[[\2][\1]]/' \
       "$input_file" > "$output_file"
-done
+  done
